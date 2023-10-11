62a13198601f722b489c002e6157dda7229fbe2d
namishiiit <namish.mamidipalli@students.iiit.ac.in>
2023-10-10 23:11:37 -0400

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Namish Mamidipalli on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
